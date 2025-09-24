013bda535a60c1d7e30e99444caa55bfede132e5
Raj <bejawadarajkumar282@gmail.com>
2025-09-24 23:02:54 +0530

    updated backend

--- SampleFrontend/BooksLibrary/src/config.js
+++ SampleFrontend/BooksLibrary/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = { 
-  url: "http://localhost:8080/api/employees" // backend base URL
+  url: "http://localhost:8081/api/employees" // backend base URL
 };
 
 export default config;
